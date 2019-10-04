d17e1d1a96ab801353319132d99fa53c5d447f32
Abdullah Ahsan <muhammad@taxfix.de>
2019-10-04 14:11:52 +0200

    Utility method to shell into a Kubernetes pod

---
+++ utility_functions.sh
@@ -0,0 +1,10 @@
+get_inside_pod() {
+	if [ -z "$1" ]
+		then
+			echo "Pod name not specified"
+			exit 1
+	fi
+	pod_name=$(kubectl get pods | grep "$1" | awk 'NR==1{print $1}')
+	echo $pod_name
+	kubectl exec -it "$pod_name" -- sh
+}
