379e02b7f3fda6099a4f0cbf05c6a3ca76b0565b
Abdullah Ahsan <muhammad@taxfix.de>
2019-10-07 10:37:57 +0200

    Added 'get_pod' function

--- utility_functions.sh
+++ utility_functions.sh
@@ -1,4 +1,10 @@
 get_inside_pod() {
+	pod_name=$(get_pod $1)
+	echo $pod_name
+	kubectl exec -it "$pod_name" -- sh
+}
+
+get_pod() {
 	if [ -z "$1" ]
 		then
 			echo "Pod name not specified"
@@ -6,5 +12,4 @@ get_inside_pod() {
 	fi
 	pod_name=$(kubectl get pods | grep "$1" | awk 'NR==1{print $1}')
 	echo $pod_name
-	kubectl exec -it "$pod_name" -- sh
 }
