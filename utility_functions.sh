30f04304b0efa9478b625f42dda2feacc637bb72
Abdullah Ahsan <muhammad@taxfix.de>
2019-10-11 13:55:11 +0200

    Switch context

--- utility_functions.sh
+++ utility_functions.sh
@@ -1,11 +1,13 @@
+#set -o xtrace
+
 shell_into_pod() {
-	pod_name=$(pod_name $1)
+	local pod_name=$(pod_name $1)
 	echo $pod_name
 	kubectl exec -it "$pod_name" -- sh
 }
 
 pod_logs() {
-	pod_name=$(pod_name $1)	
+	local pod_name=$(pod_name $1)	
 	echo $pod_name
 	kubectl logs -f --tail=10 "$pod_name" | jq
 }
@@ -16,7 +18,7 @@ pod_name() {
 			echo "Pod name not specified"
 			exit 1
 	fi
-	pod_name=$(kubectl get pods | grep "$1" | awk 'NR==1{print $1}')
+	local pod_name=$(kubectl get pods | grep "$1" | awk 'NR==1{print $1}')
 	echo $pod_name
 }
 
@@ -27,3 +29,14 @@ ctx_current() {
 ctx_list() {
 	kubectl config get-contexts -o=name
 }
+
+ctx_switch() {
+	if [ -z "$1" ]
+		then
+			echo "Context not specified"
+			exit 1
+	fi
+	local context_full_name=$(ctx_list | grep "$1")
+	kubectl config use-context $context_full_name	
+}
+
